91420f695f6a9c151636241a4238dcbb548fc067
Isaac Wassouf <isaacwassouf@gmail.com>
2025-11-03 16:53:40 +0100

    WIP: add command

--- cmd/add.go
+++ cmd/add.go
@@ -2,22 +2,21 @@ package cmd
 
 import (
 	"log"
+	"os"
 	"os/exec"
-	"time"
+	"path"
 
-	getter "github.com/hashicorp/go-getter"
 	"github.com/spf13/cobra"
 )
 
-var gitGetter = &getter.GitGetter{
-	Timeout: 5 * time.Minute,
-}
 var repo string
+var toolReposPath string
 
 var addCmd = &cobra.Command{
 	Use: "add",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmdClone := exec.Command("git", "clone", repo, "./somethinging")
+		repoPath := path.Join(toolReposPath, "testing")
+		cmdClone := exec.Command("git", "clone", repo, repoPath)
 		if err := cmdClone.Run(); err != nil {
 			log.Fatal(err)
 		}
@@ -25,6 +24,12 @@ var addCmd = &cobra.Command{
 }
 
 func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	toolReposPath = path.Join(homeDir, ".config", "switchie", "repos")
+
 	addCmd.Flags().StringVarP(&repo, "repo", "r", "", "Repo that contains the config")
 	addCmd.MarkFlagRequired("repo")
 
